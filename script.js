a7da8355d1ca8a220b2098c96b27ff0f1889616d
AThornton <antoniothornton01@gmail.com>
2023-01-26 09:04:50 -0600

    Update

---
+++ script.js
@@ -0,0 +1,34 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
+
+  
+
+
+
+const header = document.querySelector('.main-header');
+
+window.addEventListener('scroll', () => {
+    const scrollPos = window.scrollY;
+    if (scrollPos > 10 ) {
+        header.classList.add('scrolled');
+    }
+    else {
+        header.classList.remove('scrolled');
+    }
+});
